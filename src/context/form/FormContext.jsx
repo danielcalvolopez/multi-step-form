fe4bc9463b16e906ced041dca6765de0c30650ea
Daniel <acidistrict@gmail.com>
2023-01-24 21:23:59 +0000

    remove unused props

--- src/context/form/FormContext.jsx
+++ src/context/form/FormContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 const FormContext = createContext({});
 
-export const FormmProvider = ({ children, formValues }) => {
+export const FormmProvider = ({ children }) => {
   const title = {
     0: "Personal Info",
     1: "Select your plan",
